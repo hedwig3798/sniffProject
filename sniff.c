4a380729b423b808d58dba5acb9a673afce0d4a3
hedwig <hedwig3798@gmail.com>
2021-12-10 18:53:54 +0900

    test

--- sniff.c
+++ sniff.c
@@ -6,25 +6,66 @@
 #include <netinet/ip.h>
 #include <netinet/if_ether.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <memory.h>
+#include <fcntl.h>
 #include <signal.h>
+#include <time.h>
+#include <pthread.h>
+//디렉토리 생성 라이브러리
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+//시스템 관련 헤더
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <netinet/ip_icmp.h>
-#include <dirent.h>
-#include <fcntl.h>
-#include <linux/if_ether.h>
-#include <sys/stat.h>
-#include <dirent.h>
+#include "dns.h"
 
-#define BUF_SIZE 65536
+#define BUFFER_SIZE 65536
+#define PATH_MAX 512
 
 #define ICMP 1
 #define TCP 6
 #define UDP 17
 #define DNS 53
 #define HTTP 80
-#define HTTPS 443
+
+typedef struct http_header{
+	unsigned char http_first[3000];
+}http_header;
+
+int rawsocket;
+int packet_num = 0;
+int remaining_data = 0;
+int max_file;
+FILE *log_file;
+FILE *log_file_dir;
+FILE *read_file = NULL;
+char filter[80];
+char filter2[80]; // source ip
+char filter3[80]; // dest ip
+char file_token[4][40];
+char file_list[10000][100];
+
+
+struct sockaddr_in source, dest;
+struct sigaction act;
+struct http_header *hh;
+
+int packet_handler(void);
+int packet_analyze(char *filter);
+int packet_list();
+
+
+void close_handler(void){
+	printf("\n=====Pcap End=====\n");
+
+//	fclose(log_file);
+	close(rawsocket);
+}
+
 void print_ethernet_header(struct ethhdr *eth, FILE *fp);
 void print_ip_header(struct iphdr * i_header, FILE *fp);
 void print_icmp_header(struct icmp * icmp_header, struct ih_idseq *ih_idseq, FILE * fp);
@@ -32,85 +73,232 @@ void print_tcp_header(struct tcphdr * tcp_header, FILE * fp);
 void print_udp_header(struct udphdr * udp_header, FILE * fp);
 void print_data(unsigned char *data, int rest_data, FILE * fp);
 
-void reset_log_data();
+void print_menu();
+void tokenizer(char str[1024]);
 
-void tokenizer(char * str);
+void make_logdir();
+void delete_logdir();
+void get_logdir();
+int rmdirs(const char *path, int force);
+int list_view(char *filters);
 
-void end_program();
-void set_filter();
-void search_packet();
-void print_packet();
-void close_handler();
 
-char result[4][100];
+int associate_file(int ch, int flag){
 
-char is_chap = 1;
-int num_packet = 0;
-int rawsocket = 0;
-char filter_protocol[3];
-char filter_source_ip[4];
-char filter_dest_ip[4];
 
-int menu(); // menu function
-int packet_chaptuer();
-FILE *log_fp;
-
-struct sigaction act;
-
-struct sockaddr_in source, dest;
-
-int main () {
-	int choose;
-
-	while(1){
-		choose = menu();
-		printf("%d \n", choose);
-		switch (choose){
-			case 1:
-				packet_chaptuer();
-				break;
-
-			case 2:
-				set_filter();
-				break;
-
-			case 3:
-				search_packet();
-				break;
-
-			case 4:
-				print_packet();
-				break;
-
-			case 5:
-				reset_log_data();
-				break;
-
-			case 6:
-				end_program();
-				return 0;
+	if(0 <= ch && ch <= packet_num){
+		tokenizer(file_list[ch]);
+		printf("연관 필터 적용 : %s \n", file_token[1]);
+		strcpy(filter2 ,file_token[1]);
 		}
+	else{
+		printf("입력값 재확인  \n");
 	}
 }
 
 
+int file_select(const struct dirent *entry)
+{
+	char temp_filter[80] = "";
+	strcpy(temp_filter, entry->d_name);
 
-int menu() {
-	int choosen = 0;
-	printf("Choose options\n");
-	printf("----------------\n");
-	printf(" 1. start capture\n");
-	printf(" 2. setting filter\n");
-	printf(" 3. search packet\n");
-	printf(" 4. print packet");
-	printf(" 5. clear data\n");
-	printf(" 6. end program\n");
-	scanf("%d", &choosen);
+	if(strstr(temp_filter, filter) && (strstr(temp_filter, filter2)))
+	{
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+void file_read(int ch)
+{
+	read_file = NULL;
+	char dir_path[120] = "./logdir/";
+	char path[120]; 
+	strcpy(path, file_list[ch]);
+	strcat(dir_path, path);
+
+	read_file = fopen(dir_path,"r");
+	printf("입력 확인 %d \n", ch);
+	printf("실행 파일  %s \n", dir_path);
+
+	if(read_file != NULL)
+	{
+		char strTemp[4096];
+		char *pStr;
+
+		while( !feof(read_file))
+		{
+			pStr = fgets(strTemp, sizeof(strTemp), read_file);
+			printf("%s", strTemp);
+		}
+		fclose(read_file);
+	}
+	else
+	{
+		printf("read_file Error \n");
+	}
+
+}
+
+void packetSelect(){
+	int input = 0;
+	printf("분석할  패킷의 프레임 번호 : ");
+	scanf(" %d", &input);
 	getchar();
-	return choosen;
+
+	if(input > packet_num || input < 0){
+		printf("잘못된 입력 \n");
+		return;
+	}
+
+	file_read(input);
+}
+
+int main(int argc, char *argv[])
+{
+	int input, end_flag = 0;
+	socklen_t len;
+
+	while(!end_flag){
+
+		print_menu();
+		printf("\ninput : ");
+		scanf("%d", &input);
+		int count = 0;
+	
+		switch(input){
+			case 1:
+				packet_handler();
+				break;
+			case 2:
+
+				packet_analyze("");
+				list_view("");
+				break;
+			case 3: 
+				packetSelect();
+				break;
+			case 4: 
+				printf("\nfilter input : ");
+				scanf("%s", filter);
+				printf("filter set...\n");
+				break;
+			case 5: 
+				printf("연관 프레임 번호를 입력하세요 : ");
+				scanf(" %d", &input);
+				getchar();
+
+				associate_file(input, 0);
+				break;
+			case 6: 
+				strcpy(filter,"");
+				strcpy(filter2,"");
+				printf("filter reset...\n");
+				break;
+			case 7: 
+				delete_logdir();
+				exit(1);
+				break;
+			default:
+				printf("Check your input\n");
+				break;
+		}
+
+	}
+
+	return 0;
+}
+
+
+void make_logdir()
+{
+	char path[] = {"./logdir"};
+	mkdir(path, 0755);
+}
+
+
+void delete_logdir()
+{
+	char path[] = {"./logdir"};
+	int result = rmdirs(path, 1);
+
+	if(result == -1){
+		printf("delete_logdir Error\n");
+	}
+}
+
+
+int rmdirs(const char *path, int force)
+{
+	DIR * dir_ptr = NULL;
+	struct dirent *file = NULL;
+	struct stat buf;
+	char filename[1024];
+
+
+	if((dir_ptr = opendir(path)) == NULL){
+		return unlink(path);
+	}
+
+
+	while((file = readdir(dir_ptr))!=NULL){
+
+		if(strcmp(file->d_name,".")==0 || strcmp(file->d_name,"..")==0){
+			continue;
+		}
+
+
+		sprintf(filename, "%s/%s", path, file->d_name);
+
+
+		if(lstat(filename,&buf)==-1){
+			continue;
+		}
+
+
+		if(S_ISDIR(buf.st_mode)){
+
+			if(rmdirs(filename, force) == -1 && !force){
+				return -1;
+			}
+		}
+
+		else if(S_ISREG(buf.st_mode) || S_ISLNK(buf.st_mode)){
+			if(unlink(filename)==-1&&!force){
+				return -1;
+			}
+			printf("파일삭제 %s \n", file->d_name);
+		}
+	}
+
+	closedir(dir_ptr);
+	return rmdir(path);
+}
+
+
+void get_logdir()
+{
+	log_file_dir = fopen("logdir_list.txt", "w");
+    	DIR *dir = opendir("logdir");
+    	if(dir == NULL)
+    	{
+        	printf("failed open\n");
+    	}
+ 
+    	struct dirent *de=NULL;
+ 
+    	while((de = readdir(dir))!=NULL)
+    	{
+
+        	fprintf(log_file_dir, "%s\n",de->d_name);
+    	}
+    	closedir(dir);
+	fclose(log_file_dir);
 } 
 
-int packet_chaptuer(){
+int packet_handler(){
 	struct sockaddr addr;
 	int addrLen = sizeof(addr);
 	int buf_len = 0;
@@ -361,148 +549,111 @@ void print_data(unsigned char *data, int rest_data, FILE * fp){
 	fprintf(fp, "\n");
 }
 
+int packet_analyze(char *filters){
+	struct dirent **namelist;
+	int plus = 0;
+	int count = 0;
+	int idx;
 
-void reset_log_data(){
-	rmdir("./logdir", 1);
+	const char *path = "./logdir";
+
+	// NULL 로 변경, 얜 전체 다 매핑 시키는 용도로 쓸 꺼다. 
+	if((count = scandir(path, &namelist, NULL, alphasort)) == -1){
+		fprintf(stderr, "%s direntory scan error\n", path);
+		return -1;
+	}
+
+	// .이나 ..을 계산에서 제외시키기 위함이다.
+	if(strcmp(filter,"")==0 && strcmp(filter2,"")==0){
+		plus = 2;
+	}
+	else{
+		plus = 2;
+	}
+
+	for(idx = plus; idx < count; idx++){
+		//파일의 이름 출력
+		//printf("%s\n", namelist[idx]->d_name);
+		strcpy(file_list[idx - plus], namelist[idx]->d_name);
+	}
+
+	printf("반환된 count : %d\n", count);
+
+	//file_list에 데이터 저장, 디버깅 완료
+
+	for(idx = 0; idx < count; idx++){
+		free(namelist[idx]);
+	}
+
+	free(namelist);
+
+	return 0;
+
+}
+
+int list_view(char *filters){
+	struct dirent **namelist;
+	int plus = 0;
+	int count = 0;
+	int idx;
+
+	const char *path = "./logdir";
+
+	printf("Filter : %s // %s  \n", filter, filter2);	
+
+	if((count = scandir(path, &namelist, file_select, alphasort)) == -1){
+		fprintf(stderr, "%s direntory scan error\n", path);
+		return -1;
+	}
+
+	// .이나 ..을 계산에서 제외시키기 위함이다.
+	if(strcmp(filter,"")==0 && strcmp(filter2,"")==0){
+		plus = 2;
+	}
+
+
+	for(idx = plus; idx < count; idx++){
+		//파일의 이름 출력
+		printf("%s\n", namelist[idx]->d_name);
+	}
+
+	printf("반환된 count : %d\n", count);
+
+	//file_list에 데이터 저장, 디버깅 완료
+
+	for(idx = 0; idx < count; idx++){
+		free(namelist[idx]);
+	}
+
+	free(namelist);
+
+	return count;
+
 }
 
 
-void tokenizer(char * str){
-	char * temp = (char*)malloc(sizeof(str) + 1);
-	char * ptr;
+void print_menu(){
+	printf("\n=====Program Menu=====\n");
+	printf("1.Capture Start \n");
+	printf("2.List View \n");
+	printf("3.Select Packet\n");
+	printf("4.set Filter \n");
+	printf("5.set associate Filter \n");
+	printf("6.reset Filter \n");
+	printf("7.exit \n");
+}
+
+void tokenizer(char str[1024]){
+	char temp[1024];
+	char *ptr;
 	int i = 0;
+
 	strcpy(temp, str);
-	
-	memset(result, 0, sizeof(result));
-
 	ptr = strtok(temp, "_");
-
-	while (ptr != NULL){
-		strcpy(result[i], ptr);
-		i ++;
+	
+	while(ptr != NULL){
+		strcpy(file_token[i], ptr);
+		i++;
 		ptr = strtok(NULL, "_");
 	}
-}
-
-void end_program(){
-	reset_log_data();
-}
-
-void init_filter(){
-	strcpy(filter_source_ip, "");
-	strcpy(filter_dest_ip, "");
-	strcpy(filter_protocol, "");
-}
-
-void set_filter(){
-	
-	init_filter();
-
-	printf("\n input source ip: ");
-	scanf("%s", filter_source_ip);
-	printf("\n input dest ip  : ");
-	scanf("%s", filter_dest_ip);
-	printf("\n input protocol : ");
-	scanf("%s", filter_protocol);
-
-	printf("\nfilter set %s, %s, %s\n", filter_source_ip, filter_dest_ip, filter_protocol);
-	
-}
-void search_packet(){
-	const char * path = "./logdir";
-	DIR * direc;
-	struct dirent * dir_entry;
-
-
-	if (
-			(strcmp(filter_source_ip, "") == 0) &&
-			(strcmp(filter_dest_ip, "") == 0) &&
-			(strcmp(filter_protocol, "") == 0)
-		){
-		printf("please set filter first\n");
-	}
-
-	direc = opendir(path);
-	while(dir_entry = readdir(direc)){
-		tokenizer(dir_entry->d_name);
-
-		if((strcmp(result[1], filter_source_ip) == 0)){
-			printf("%s\n", dir_entry->d_name);
-		}
-		else if ((strcmp(result[2], filter_dest_ip) == 0)){
-			printf("%s\n", dir_entry->d_name);
-		}
-		else if ((strcmp(result[3], filter_protocol) == 0)){
-			printf("%s\n", dir_entry->d_name);
-		}
-		
-	}
-
-
-}
-
-void print_packet(){
-	int num;
-	char frame[10];
-
-
-	printf("input packet number: ");
-	scanf("%d", &num);
-
-	if (num < 10){
-		sprintf(frame, "000%d", num);
-	}
-	else if (10 <= num && num < 100){
-		sprintf(frame, "00%d", num);
-	}
-	else if (100 <= num && num < 1000){
-		sprintf(frame, "0%d", num);
-	}
-	else if (1000 <= num && num < 10000){
-		sprintf(frame, "%d", num);
-	}
-
-	const char * path = "./logdir";
-	DIR * direc;
-	struct dirent * dir_entry;
-	char file_name[100];
-	direc = opendir(path);
-	strcpy(file_name, "");
-	while(dir_entry = readdir(direc)){
-		tokenizer(dir_entry->d_name);
-
-		if((strcmp(result[0], frame) == 0)){
-			printf("%s\n", dir_entry->d_name);
-			strcpy(file_name, dir_entry->d_name);
-			break;
-		}
-
-		if (strcmp(file_name, "") == 0){
-			printf("cannot find file\n");
-			return;
-		}
-	}	
-
-	char real_file[100];
-	
-	strcpy(real_file, path);
-
-	sprintf(real_file, "%s", file_name);
-				
-		
-	char temp[10000] = {0, };
-	FILE * fp;
-	fp = fopen(real_file, "r");
-
-	fread(temp, sizeof(temp), 1, fp);
-
-	
-	fclose(fp);
-
-
-}
-
-void close_handler(){
-	printf("\nChapture end\n");
-	is_chap = 0;
 }
